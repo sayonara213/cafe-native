7de723c071ab4d119b772521ce6ea6ecbac4d7b5
sayonara213 <79828791+sayonara213@users.noreply.github.com>
2023-04-06 14:18:10 +0300

    init of theme

---
+++ src/screens/auth/auth.styled.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components/native';
+
+export const AuthText = styled.Text`
+  font-size: ${({ theme }) => theme.fontSize.medium};
+  font-family: ${({ theme }) => theme.fontFamily.black};
+`;
--- src/screens/auth/auth.tsx
+++ src/screens/auth/auth.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { AuthText } from './auth.styled';
 
 const Auth: React.FC = () => {
   return (
     <View>
-      <Text>123</Text>
+      <AuthText>123</AuthText>
     </View>
   );
 };
